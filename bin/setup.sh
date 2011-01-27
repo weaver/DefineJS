9c53df8703cf4aee0d3df62184f6e3f0dafa3946
Ben Weaver <ben@orangesoda.net>
2011-01-27 15:08:08 -0500

    Register modules in the module cache. Add support for renode.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,13 +4,15 @@ zero=`python -c "import os; print os.path.realpath('$0')"`
 here=`dirname $zero`
 base=`dirname $here`
 
-script="$base/bin/defjs"
-dest=/usr/local/bin/defjs
+scripts="$base/bin"
+dest="/usr/local/bin"
 
 ## Install DefineJS ##
 
 set -x
 
-sudo ln -sf $script $dest
+for name in defjs redef; do
+    sudo ln -sf $scripts/$name $dest/$name
+done
 
 set +x
